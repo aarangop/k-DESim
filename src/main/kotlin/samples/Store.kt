c7376c796778269060601dc05a539b4c4932205f
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2023-03-31 16:04:45 +0200

    Write KDocs for Store

---
+++ src/main/kotlin/samples/Store.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2023. Andrés Arango Pérez <arangoandres.p@gmail.com>
+ *
+ * You may use, distribute and modify this code under the terms of the MIT license.
+ */
+
+package samples
+
+// TODO: Write example of Store usage.
