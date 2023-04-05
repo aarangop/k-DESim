a33de95c1ad4090d61983235d0040b98bc7acbea
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2023-04-05 16:48:30 +0200

    Re-structure project

--- core/src/test/kotlin/simulation/resources/StoreTest.kt
+++ core/src/test/kotlin/simulation/resources/StoreTest.kt
@@ -132,4 +132,10 @@ class StoreTest : KDESimTestBase() {
 
         assertEquals(EventValueStatus.AVAILABLE, putRequest!!.valueStatus)
     }
+
+    @Test
+    fun `concurrent requests for a resource must wait for the resource to be released`() {
+        // TODO: test concurrent access to a single resource, e.g. when multiple entities request access to a server each one must wait for the server to be released.
+        assertTrue { false }
+    }
 }
